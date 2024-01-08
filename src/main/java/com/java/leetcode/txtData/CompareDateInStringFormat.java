ee19715f6d8cbefe678ccf9de93a76d1e171eef1
Sunil Gupta <sunilg@infinite.com>
2024-01-08 17:43:03 +0530

    Begin Date ANd ENd Date Is COmpleted

---
+++ src/main/java/com/java/leetcode/txtData/CompareDateInStringFormat.java
@@ -0,0 +1,14 @@
+package com.java.leetcode.txtData;
+
+public class CompareDateInStringFormat {
+	public static void main(String[] args) {
+		String date = "2010-12-12";
+		String date1 = "2010-12-11";
+		if(date.equals(date1)) {
+			System.out.println("Both Are Same");
+		}else {
+			System.out.println("Not Equal");
+		}
+	}
+
+}
---
+++ src/main/java/com/java/leetcode/txtData/DateBeginDateAndEndDate.java
@@ -0,0 +1,30 @@
+package com.java.leetcode.txtData;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class DateBeginDateAndEndDate {
+
+    public static void main(String[] args) {
+        String beginDateStr = "2023-01-15";
+        String endDateStr = "2023-02-10";
+        String date1 = "";
+
+        // Define a date format matching your string representation
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+        // Parse the strings into LocalDate objects
+        LocalDate beginDate = LocalDate.parse(beginDateStr, formatter);
+        LocalDate endDate = LocalDate.parse(endDateStr, formatter);
+
+        // Check if beginDate is greater than endDate
+        if (beginDate.isAfter(endDate)) {
+            System.out.println("Begin date should not be greater than end date.");
+            // Handle the situation where the begin date is greater than the end date
+        } else {
+            System.out.println("Dates are valid: Begin date is not greater than end date.");
+            // Proceed with your logic if the dates are valid
+        }
+    }
+}
+
