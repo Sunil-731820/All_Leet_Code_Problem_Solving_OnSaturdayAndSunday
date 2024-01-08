cdd0989b4a0e355bb1fa4c6dc33860e2818f2783
Sunil Gupta <sunilg@infinite.com>
2024-01-08 14:17:30 +0530

    Rotation problem is COmpleted

--- src/main/java/com/java/leetcode/RotateArrayProblem.java
+++ src/main/java/com/java/leetcode/RotateArrayProblem.java
@@ -1,10 +1,30 @@
 package com.java.leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class RotateArrayProblem {
 	
 	public static void RotateElementAccodingToUser(int[] numbers, int rotation) {
+		System.out.println("Inside the RotateArray method is getting called : ");
+		List<Integer> leftList = new ArrayList<Integer>();
+		List<Integer> rightList = new ArrayList<Integer>();
+		for(int i=0;i<numbers.length;i++) {
+			if(i<rotation) {
+				leftList.add(numbers[i]);
+			}else {
+				rightList.add(numbers[i]);
+			}
+		}
+		System.out.println("The Left List is After Adding The till Rotation Element is ");
+		System.out.println(leftList);
+		System.out.println("The Right list is After Adding The from Rotation To end is : =");
+		System.out.println(rightList);
+		System.out.println("Now Adding The Right into The left Portions ");
+		rightList.addAll(leftList);
+		System.out.println("After Adding The right List is : ");
+		System.out.println(rightList);
 		
 	}
 	
@@ -21,6 +41,7 @@ public class RotateArrayProblem {
 		}
 		System.out.println("Enter The Element for number of Rotation : ");
 		int rotateElement = sc.nextInt();
+		RotateElementAccodingToUser(arr, rotateElement);
 		
 	}
 
